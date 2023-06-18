0019df99b803e7ed1da2a5b197323d84ed7cb327
@hexlet/cli <support@hexlet.io>
2023-06-18 16:15:11 +0300

    submit java-spring-ru/spring-lifecycle

--- java-spring-ru/spring-lifecycle/src/main/java/exercise/Meal.java
+++ java-spring-ru/spring-lifecycle/src/main/java/exercise/Meal.java
@@ -3,21 +3,19 @@ package exercise;
 import jakarta.annotation.PostConstruct;
 
 // BEGIN
+import org.springframework.stereotype.Component;
 
+@Component
 // END
 public class Meal {
     public String getMealForDaytime(String daytime) {
 
-        switch (daytime) {
-            case "morning":
-                return "breakfast";
-            case "day":
-                return "lunch";
-            case "evening":
-                return "dinner";
-            default:
-                return "nothing :)";
-        }
+        return switch (daytime) {
+            case "morning" -> "breakfast";
+            case "day" -> "lunch";
+            case "evening" -> "dinner";
+            default -> "nothing :)";
+        };
     }
 
     // Для самостоятельной работы
